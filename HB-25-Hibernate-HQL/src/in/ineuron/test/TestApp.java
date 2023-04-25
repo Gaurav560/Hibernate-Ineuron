a26d77f0b8176df79deff569eb292c838665e93c
GAURAV SHARMA <gsharma1154@gmail.com>
2023-04-26 02:17:56 +0530

    HQL

---
+++ HB-25-Hibernate-HQL/src/in/ineuron/test/TestApp.java
@@ -0,0 +1,43 @@
+package in.ineuron.test;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import in.ineuron.model.products;
+import in.ineuron.util.hibernateUtil;
+
+public class TestApp {
+
+	public static void main(String[] args)
+
+	{
+
+//for select operation in hibernate no transaction is required
+		Session session = null;
+		try
+
+		{
+			session = hibernateUtil.getSession();
+			// prepare query object to hold hql
+			Query query = session.createQuery("FROM in.ineuron.model.products");// works same as selct * from products
+			
+			//execute the query
+			List<products> li = query.list();
+			
+			//process the List Object 
+			li.forEach(System.out::println);
+			
+
+		}
+
+		catch (HibernateException he) {
+			he.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
